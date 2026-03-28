72449d7679b6dd12ded210d181b5691d74864356
Chong Bin Wee <34395942+cbwee@users.noreply.github.com>
2026-03-29 06:20:57 +0800

    Update Google Param Reorder.js

--- Google Param Reorder.js
+++ Google Param Reorder.js
@@ -1,15 +1,15 @@
 // ==UserScript==
-// @name         Google Param Reorder
-// @version      1.0
+// @name         Google Param Reorder
+// @version      1.0
 // @description
-// @match        https://www.google.com/search*
-// @grant        none
-// @run-at       document-start
-// ==/UserScript==
+// @match        https://www.google.com/search*
+// @grant        none
+// @run-at       document-start
+// ==UserScript==
 
 (function (l) {
-  var s = l.search;
-  if (!s || s.indexOf("?q=") === 0) return;
-  var m = s.match(/[?&](q=[^&]+)/);
-  if (m) history.replaceState(null, "", "?" + m[1] + s.replace(m[0], "").replace("?", "&") + l.hash);
+  var s = l.search, i = s.indexOf("&q=");
+  if (i === -1) return;
+  var e = s.indexOf("&", i + 3), q = s.substring(i + 1, e === -1 ? s.length : e);
+  history.replaceState(null, "", "?" + q + s.replace("&" + q, "").replace("?", "&") + l.hash);
 })(location);
